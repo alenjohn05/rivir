102ee295e65c12b31372148a17415d0c55bf05f8
alenjohn05 <alenjohnmj@gmail.com>
2023-12-12 10:30:00 +0530

    updated the main files

---
+++ src/main/java/com/spring_basic_auth/basic_auth/BasicAuthApplication.java
@@ -0,0 +1,17 @@
+package com.spring_basic_auth.basic_auth;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class BasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthApplication.class, args);
+
+	}
+}
